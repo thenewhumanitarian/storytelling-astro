467f251375ab6533d1d2623ad4b7837964b3879f
thenewhumanitarian <systemadmin@thenewhumanitarian.org>
2023-08-23 15:17:51 +0200

    font pre-loading

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwind from '@astrojs/tailwind'
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind({ applyBaseStyles: false })],
-})
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), prefetch()]
+});
