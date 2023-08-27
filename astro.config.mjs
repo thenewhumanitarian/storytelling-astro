a4859b15e38de82293be0f33e0a1557ba974a5c3
thenewhumanitarian <systemadmin@thenewhumanitarian.org>
2023-08-27 09:43:37 +0200

    Adds infinite grid example

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
-import prefetch from "@astrojs/prefetch";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import prefetch from '@astrojs/prefetch'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), prefetch()]
-});
+	integrations: [
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		prefetch(),
+	],
+})
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -20,5 +20,5 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [require('@tailwindcss/typography'), require('@tailwindcss/line-clamp')],
+	plugins: [require('@tailwindcss/typography')],
 }
