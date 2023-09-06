b26881b93013387758f841b6d76261ac1d95d030
thenewhumanitarian <systemadmin@thenewhumanitarian.org>
2023-09-06 20:23:36 +0200

    adds transition and upgrades astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import prefetch from '@astrojs/prefetch'
 
-import image from '@astrojs/image'
-
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
@@ -11,8 +9,5 @@ export default defineConfig({
 			applyBaseStyles: false,
 		}),
 		prefetch(),
-		image({
-			serviceEntryPoint: '@astrojs/image/sharp',
-		}),
 	],
 })
